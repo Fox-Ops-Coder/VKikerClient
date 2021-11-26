324fa73396a628c3fc4c5ebc5ce9fa34f9e758bc
Fox-Dev-Ops <mwo2016@inbox.ru>
2021-11-26 16:26:06 +0300

    Fix

--- VKikerModule/vkiker-module/src/main/java/com/foxdev/vkikermodule/net/Endpoints.java
+++ VKikerModule/vkiker-module/src/main/java/com/foxdev/vkikermodule/net/Endpoints.java
@@ -1,6 +1,6 @@
 package com.foxdev.vkikermodule.net;
 
-final class Endpoints {
+public final class Endpoints {
     public static final String LEADERBOARD_ENDPOINT = "/leaderboard";
     public static final String GET_USER = "/users?user=userId";
 }
