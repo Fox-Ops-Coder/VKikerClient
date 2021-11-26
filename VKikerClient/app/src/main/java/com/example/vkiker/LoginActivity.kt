423e2df50218c75edf2162c34f9f03bd4b1e116a
InarBelkin <Inar2000s@mail.ru>
2021-11-26 17:38:59 +0300

    i have fcm token

--- VKikerClient/app/src/main/java/com/example/vkiker/LoginActivity.kt
+++ VKikerClient/app/src/main/java/com/example/vkiker/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.vkiker
 
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -8,8 +9,10 @@ import androidx.databinding.DataBindingUtil
 import com.example.vkiker.databinding.ActivityLoginBinding
 import com.example.vkiker.databinding.ActivityMainBinding
 import android.content.SharedPreferences
-
-
+import android.util.Log
+import com.foxdev.vkikermodule.current.CurrentUser
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 
 class LoginActivity : AppCompatActivity() {
@@ -20,8 +23,26 @@ class LoginActivity : AppCompatActivity() {
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
         val storageName = "LoginStorage"
         val mySharedPreferences = getSharedPreferences(storageName, Context.MODE_PRIVATE);
+        val user = CurrentUser(mySharedPreferences);
+        val userId = user.currentUser;
 
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent);
+        if (userId != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent);
+        } else {
+            FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener {
+                if (!it.isSuccessful) {
+                    Log.w(ContentValues.TAG, "Fetching FCM registration token failed", it.exception)
+
+                }
+                val token = it.result
+                Log.d(ContentValues.TAG, "Token= $token");
+
+            })
+
+
+        }
+
+
     }
 }
