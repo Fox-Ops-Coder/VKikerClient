f8738f6339d2ce44aae01bf0d7d8725dc7917319
InarBelkin <Inar2000s@mail.ru>
2021-12-07 15:49:07 +0300

    Change ELO in LeaderInfo back to float

--- VKikerModule/vkiker-module/src/main/java/com/foxdev/vkikermodule/net/netobjects/LeaderInfo.java
+++ VKikerModule/vkiker-module/src/main/java/com/foxdev/vkikermodule/net/netobjects/LeaderInfo.java
@@ -10,7 +10,7 @@ public final class LeaderInfo {
     public User user;
 
     @SerializedName("elo")
-    public int ELO;
+    public float ELO;
 
     @SerializedName("battles")
     public int Battles;
